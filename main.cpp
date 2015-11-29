99cee5a85f3e77319384ac47093ccbb6f3763132
Pat <houghstler@gmail.com>
2015-11-29 14:51:50 -0500

    added method skeletons
    and brief comments for each

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 Program Flow:
 	
-	Input text thats copied from a file, given to the program via stdin.
-	program removes stop words, stems the words and indexes them
-	program then gets a query (through std in)
-	program removes stop words from the query, and stems remaining words
-	program returns the amount of times that the query appears in given document
+Input text thats copied from a file, given to the program via stdin.
+program removes stop words, stems the words and indexes them
+program then gets a query (through std in)
+program removes stop words from the query, and stems remaining words
+program returns the amount of times that the query appears in given document
 
 */
 
@@ -20,13 +20,32 @@ void getFile() {
 	
 }
 
-//This reads in the user query from stdin
+// this removes stop words from the input (and the query)
+void stopwords() {
+	
+}
+
+// This stems the string that it is given
+void stem(){
+	
+}
+
+// this indexes the remaining words from the given input
+void index() {
+	
+}
+
+// This reads in the user query from stdin
 void getQuery () {
 	
 }
 
+// This returns the amount of times that the query occurred in the doc
+void returnResults(){
+	
+}
+
+// Main method
 int main() {
-	
-	
 	
 }
