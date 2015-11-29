c191b1e734443cd5da0cba26d9621b293d012025
Pat <houghstler@gmail.com>
2015-11-29 14:34:19 -0500

    added files
    stuff

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+
+using namespace std;
+
+//Plan out how this shit is going to work.
+/*
+
+User starts program and enters the file in as a stdin input. (Since this stupid tool can't read files)
+Program takes the text given to it, removes the stop words, stems the remaining words.
+Then the programs 
+
+
+
+
+
+
+
+*/
+
+// This reads in an input file from stdin
+void getFile() {
+	
+}
+
+//This reads in the user query from stdin
+void getQuery () {
+	
+}
+
+int main() {
+	
+	
+	
+}
