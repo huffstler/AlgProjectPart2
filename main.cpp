5fb867b32257ab4036d844a262d3074d1c5acf30
Pat <houghstler@gmail.com>
2015-11-29 20:40:10 -0500

    unzipped file for easy access to files
    yup

--- main.cpp
+++ main.cpp
@@ -24,13 +24,14 @@ using namespace std;
  program returns the amount of times that the query appears in given document
  */
 
-vector<string> stopWords = {"a","about","above","across","after","afterwards","again","against","all","almost","alone","along","already","also","although","always","am","among","amongst","amoungst","amount","an","and","another","any","anyhow","anyone","anything","anyway","anywhere","are","around","as","at","back","be","became","because","become","becomes","becoming","been","before","beforehand","behind","being","below","beside","besides","between","beyond","bill","both","bottom","but","by","call","can","cannot","cant","co","computer","con","could","couldnt","cry","de","describe","detail","do","done","down","due","during","each","eg","eight","either","eleven","else","elsewhere","empty","enough","etc","even","ever","every","everyone","everything","everywhere","except","few","fifteen","fify","fill","find","fire","first","five","for","former","formerly","forty","found","four","from","front","full","further","get","give","go","had","has","hasnt","have","he","hence","her","here","hereafter","hereby","herein","hereupon","hers","him","his","how","however","hundred","i","ie","if","in","inc","indeed","interest","into","is","it","its","keep","last","latter","latterly","least","less","ltd","made","many","may","me","meanwhile","might","mill","mine","more","moreover","most","mostly","move","much","must","my","name","namely","neither","never","nevertheless","next","nine","no","nobody","none","noone","nor","not","nothing","now","nowhere","of","off","often","on","once","one","only","onto","or","other","others","otherwise","our","ours","ourselves","out","over","own","part","per","perhaps","please","put","rather","re","same","see","seem","seemed","seeming","seems","serious","several","she","should","show","side","since","sincere","six","sixty","so","some","somehow","someone","something","sometime","sometimes","somewhere","still","such","system","take","ten","than","that","the","their","them","themselves","then","thence","there","thereafter","thereby","therefore","therein","thereupon","these","they","thick","thin","third","this","those","though","three","through","throughout","thru","thus","to","together","too","top","toward","towards","twelve","twenty","two","un","under","until","up","upon","us","very","via","was","we","well","were","what","whatever","when","whence","whenever","where","whereafter","whereas","whereby","wherein","whereupon","wherever","whether","which","while","whither","who","whoever","whole","whom","whose","why","will","with","within","without","would","yet","you","your","yours","yourself","yourselves"};
 unordered_map<int,string> mymap;
+list<string> importedData;
 hash<string> strhash;
+
+vector<string> stopWords = {"a","about","above","across","after","afterwards","again","against","all","almost","alone","along","already","also","although","always","am","among","amongst","amoungst","amount","an","and","another","any","anyhow","anyone","anything","anyway","anywhere","are","around","as","at","back","be","became","because","become","becomes","becoming","been","before","beforehand","behind","being","below","beside","besides","between","beyond","bill","both","bottom","but","by","call","can","cannot","cant","co","computer","con","could","couldnt","cry","de","describe","detail","do","done","down","due","during","each","eg","eight","either","eleven","else","elsewhere","empty","enough","etc","even","ever","every","everyone","everything","everywhere","except","few","fifteen","fify","fill","find","fire","first","five","for","former","formerly","forty","found","four","from","front","full","further","get","give","go","had","has","hasnt","have","he","hence","her","here","hereafter","hereby","herein","hereupon","hers","him","his","how","however","hundred","i","ie","if","in","inc","indeed","interest","into","is","it","its","keep","last","latter","latterly","least","less","ltd","made","many","may","me","meanwhile","might","mill","mine","more","moreover","most","mostly","move","much","must","my","name","namely","neither","never","nevertheless","next","nine","no","nobody","none","noone","nor","not","nothing","now","nowhere","of","off","often","on","once","one","only","onto","or","other","others","otherwise","our","ours","ourselves","out","over","own","part","per","perhaps","please","put","rather","re","same","see","seem","seemed","seeming","seems","serious","several","she","should","show","side","since","sincere","six","sixty","so","some","somehow","someone","something","sometime","sometimes","somewhere","still","such","system","take","ten","than","that","the","their","them","themselves","then","thence","there","thereafter","thereby","therefore","therein","thereupon","these","they","thick","thin","third","this","those","though","three","through","throughout","thru","thus","to","together","too","top","toward","towards","twelve","twenty","two","un","under","until","up","upon","us","very","via","was","we","well","were","what","whatever","when","whence","whenever","where","whereafter","whereas","whereby","wherein","whereupon","wherever","whether","which","while","whither","who","whoever","whole","whom","whose","why","will","with","within","without","would","yet","you","your","yours","yourself","yourselves"};
 
 // This reads in an input file from stdin
 void getFile(){ // Done
-    list<string> importedData;
     string tempStringP;
     int count = 0;
     while (getline(cin, tempStringP, '\n')){ //reads in a line from ideone
@@ -39,31 +40,11 @@ void getFile(){ // Done
         }
     }
     
-        stringstream iss(tempStringP);
-        while (getline(iss, tempStringP, ' ')){ //get string up to a space
-            tempStringP.erase(0,1);
-            importedData.push_back(tempStringP);
-            count++;
-        }
-    
-    if(!importedData.empty())
-    {
-        auto iter = importedData.begin();
-        while(true)
-        {
-            cout << *iter;
-            ++iter;
-            if(iter == importedData.end())
-            {
-                break;
-            }
-            else
-            {
-                cout << ", ";
-            }
-        }
-    }
-             //  cout << importedData << " ";
+	stringstream iss(tempStringP);
+	while (getline(iss, tempStringP, ' ')){ //get string up to a space
+		importedData.push_back(tempStringP);
+		count++;
+	}
 }
 
 void populateMap() { // Done
@@ -73,8 +54,26 @@ void populateMap() { // Done
 }
 
 // this removes stop words from the input (and the query)
-void rmStopwords() {
-	
+void rmStopWords() {
+	if(!importedData.empty()) {
+		auto iter = importedData.begin();
+		
+        while(true) {
+            cout << *iter;
+            ++iter;
+			
+            if(iter == importedData.end()) {
+                break;
+            }
+			
+            else {
+                
+            }
+        }
+    }
+	else{
+		cout << "There was nothing given in stdin. Exiting." << endl;
+	}
 }
 
 // This stems the string that it is given
@@ -101,8 +100,8 @@ void returnResults(){
 int main() {
 	
     populateMap(); // puts the stop words in a map for quick access
-    importData(); // reads std for the file to be input.
+    getFile(); // reads std for the file to be input.
 	
-	
+	rmStopWords();
     
 }
