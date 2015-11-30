5b7c657255cc9e28e99c1c2216c386c59598eb72
Pat <houghstler@gmail.com>
2015-11-30 01:47:31 -0500

    working on string manipulation
    It sucks. Spoiler alert

--- main.cpp
+++ main.cpp
@@ -29,6 +29,10 @@ hash<string> strhash;
 
 vector<string> stopWords = { "a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "computer", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "him", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves" };
 
+int offset;
+int start=0;
+int end;
+
 //Is it a stop word?
 bool isStop(string word){
 	if (mymap.find(strhash(word)) != mymap.end()) {
@@ -83,7 +87,7 @@ void getFile(){
 	if (!importedData.empty()) {
 		auto iter = importedData.begin();
 
-		while (true) {
+		while (true) { // call stem here
 			cout << *iter;
 			++iter;
 
@@ -106,10 +110,13 @@ void populateMap() { // Done
 }
 
 // This stems the string that it is given
-void stem(string input) {  
+void stem(string *input) {  
+	end = input.size() - 1;
+	offset = end - start; // kinda stupid, I know
+	
     if (input.size() > 2) {
-		/* step1a();
-		step1b();
+		step1a(*input);
+	  /*step1b();
 		step1c(); 
 		step2(); 
 		step3(); 
@@ -117,6 +124,56 @@ void stem(string input) {
 		step5(); */
 	}
 }
+
+/* static char * b;
+static int k,k0,j;       j is a general offset into the string 
+		k = end index k0 starting index, j is the size I guess */
+void step1a(string *input){
+	//check if it ends with 's'
+	if (*input.back() == 's'){
+		if(*input.substr(end-3, 3) == "sses") { //ends in "sses"
+			end -= 2;
+		}
+		else if (*input.substr(end-2, 2) == "ies") { //ends in "ies"
+			*input.replace(substr(end-2, 2), "i");
+		}
+		else if (*input[end-1] != 's') { // second to last letter isn't an s. (ends in C's' where C is any consonant
+			end --;
+		}
+	//check if it ends in "eed"
+	} else if (*input.substr(end-2, 2) == "eed") {
+		
+	//check if it ends in "ed"
+	} else if () {
+		
+	}
+}
+
+void step1b(string *input){
+	
+}
+
+void step1c(string *input){
+	
+}
+
+void step2(string *input){
+	
+}
+
+void step3(string *input){
+	
+}
+
+void step4(string *input){
+	
+}
+
+void step5(string *input){
+	
+}
+
+
 
 // this indexes the remaining words from the given input
 void index() {
