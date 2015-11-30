a69eac8ca9a4e497a05c9c8553f922bf429ea5e2
Pat <houghstler@gmail.com>
2015-11-29 23:29:08 -0500

    Cleaned up code ALOT

--- main.cpp
+++ main.cpp
@@ -28,48 +28,40 @@ unordered_map<int,string> mymap;
 list<string> importedData;
 hash<string> strhash;
 
-
-
-
 vector<string> stopWords = {"a","about","above","across","after","afterwards","again","against","all","almost","alone","along","already","also","although","always","am","among","amongst","amoungst","amount","an","and","another","any","anyhow","anyone","anything","anyway","anywhere","are","around","as","at","back","be","became","because","become","becomes","becoming","been","before","beforehand","behind","being","below","beside","besides","between","beyond","bill","both","bottom","but","by","call","can","cannot","cant","co","computer","con","could","couldnt","cry","de","describe","detail","do","done","down","due","during","each","eg","eight","either","eleven","else","elsewhere","empty","enough","etc","even","ever","every","everyone","everything","everywhere","except","few","fifteen","fify","fill","find","fire","first","five","for","former","formerly","forty","found","four","from","front","full","further","get","give","go","had","has","hasnt","have","he","hence","her","here","hereafter","hereby","herein","hereupon","hers","him","his","how","however","hundred","i","ie","if","in","inc","indeed","interest","into","is","it","its","keep","last","latter","latterly","least","less","ltd","made","many","may","me","meanwhile","might","mill","mine","more","moreover","most","mostly","move","much","must","my","name","namely","neither","never","nevertheless","next","nine","no","nobody","none","noone","nor","not","nothing","now","nowhere","of","off","often","on","once","one","only","onto","or","other","others","otherwise","our","ours","ourselves","out","over","own","part","per","perhaps","please","put","rather","re","same","see","seem","seemed","seeming","seems","serious","several","she","should","show","side","since","sincere","six","sixty","so","some","somehow","someone","something","sometime","sometimes","somewhere","still","such","system","take","ten","than","that","the","their","them","themselves","then","thence","there","thereafter","thereby","therefore","therein","thereupon","these","they","thick","thin","third","this","those","though","three","through","throughout","thru","thus","to","together","too","top","toward","towards","twelve","twenty","two","un","under","until","up","upon","us","very","via","was","we","well","were","what","whatever","when","whence","whenever","where","whereafter","whereas","whereby","wherein","whereupon","wherever","whether","which","while","whither","who","whoever","whole","whom","whose","why","will","with","within","without","would","yet","you","your","yours","yourself","yourselves"};
-
 
 //Is it a stop word?
 bool isStop (string word){
-    if(mymap.find(strhash(word)) != mymap.end()){
+    if(mymap.find(strhash(word)) != mymap.end()) {
         return true;
-    }
-    else{
+    } else {
         return false;
     }
 }
 
-
 // This reads in an input file from stdin
 // Reads past new lines now
-//Everything works, DO NOT TOUCH!!!!!
 void getFile(){
-    //string importedData[500];
     list<string> importedData;
     string tempStringP;
-    int count = 0;
-    //unordered_map<string, int> m;
+    //int count = 0;
+
     while (getline(cin, tempStringP )){ //reads in a line from ideone
         
-        for (int i = 0; tempStringP[i] != '\0'; i++){//lowercase everything
+        for (int i = 0; tempStringP[i] != '\0'; i++) {//lowercase everything
             tempStringP[i] = tolower(tempStringP[i]);
         }
         
-        
         stringstream iss(tempStringP);
-        while (getline(iss, tempStringP, ' ')){ //get string up to a space
+        
+		while (getline(iss, tempStringP, ' ')){ //get string up to a space
             
             //This removes periods
             unsigned long len = tempStringP.size();
-            for (unsigned long i = 0; i < len; i++)
-            {
-                if (ispunct(tempStringP[i]))
-                {
+			
+            for (unsigned long i = 0; i < len; i++) {
+				
+                if (ispunct(tempStringP[i])) {
                     tempStringP.erase(i--, 1);
                     len = tempStringP.size();
                 }
@@ -79,26 +71,23 @@ void getFile(){
             if(!isStop(tempStringP)) {
                 importedData.push_back(tempStringP);
             }
-            count++;
+            //count++;
         }
     }
     
     //This prints my imported data so I can see that it is correct
     //comment in and out as needed
     
-    if(!importedData.empty())
-    {
+    if(!importedData.empty()) {
         auto iter = importedData.begin();
-        while(true)
-        {
+		
+        while(true) {
             cout << *iter;
             ++iter;
-            if(iter == importedData.end())
-            {
+			
+            if(iter == importedData.end()) {
                 break;
-            }
-            else
-            {
+            } else {
                 cout << ", ";
             }
         }
@@ -112,41 +101,6 @@ void populateMap() { // Done
         mymap.insert(make_pair(strhash(stopWords[i]),stopWords[i]));
     }
 }
-
-
-
-// this removes stop words from the input (and the query)
-//Currently not using this, instead only adding non stop words when parsing in getFile()
-/*
- void rmStopWords() {
- list<string> myOtherList;
-	if(!importedData.empty()) {
- auto iter = importedData.begin();
- 
- while(true) {
- if (isStop(*iter)){ // Means that the word is a stop word
- cout << "\"" << *iter << "\"" <<  " IS a stop word" << endl;
- 
- //importedData.erase(iter); // remove the word from the list
- ++iter;
- }
- 
- else {
- cout << "\"" <<*iter<< "\"" << " is NOT a stop word" << endl;
- myOtherList.push_back(*iter);
- ++iter;
- }
- if(iter == importedData.end()) {
- cout << "at the end." << endl;
- break;
- }
- }
- }
-	else{
- cout << "There was nothing given in stdin. Exiting." << endl;
-	}
- }
- */
 
 // This stems the string that it is given
 void stem(){
@@ -173,7 +127,5 @@ int main() {
     
     populateMap(); // puts the stop words in a map for quick access
     getFile(); // reads std for the file to be input.
-    
-    //rmStopWords();
     
 }
