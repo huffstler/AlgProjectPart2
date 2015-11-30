aebaf676b7804aea79b59e608de2ab3c8cbb0871
Pat <houghstler@gmail.com>
2015-11-30 02:46:52 -0500

    a lot of my stuff got clobbered last time I pulled
    fingers crossed

--- main.cpp
+++ main.cpp
@@ -30,6 +30,8 @@ hash<string> strhash;
 
 vector<string> stopWords = { "a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "computer", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "him", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves" };
 
+int frnt = 0 , back, off;
+
 //Is it a stop word?
 bool isStop(string word){
 	if (mymap.find(strhash(word)) != mymap.end()) {
@@ -109,8 +111,13 @@ void populateMap() { // Done
 
 // This stems the string that it is given
 void stem(string *input){
-	if (*input.length() > 2) {
+	back = *input.length() - 1;
+	off = back - frnt; // kinda stupid, I know
+	
+	if (back + 1 > 2) {
 		step1a(*input);
+		step1b(*input);
+		step2(*input);
 	}
 }
 
@@ -249,6 +256,22 @@ void step1b (string *input) {
 	}
 }
 
+void step2 (string *input) {
+	switch (*input.at(end-1)){
+		case 'a':
+			if 
+			
+			break
+		case 'c':
+		case 'e':
+		case 'l':
+		case 'o':
+		case 's':
+		case 't':
+		//case 'g':
+	}
+	
+}
 // This reads in the user query from stdin
 void getQuery() {
 
